81abeb7b8f695cec10d5edfb99a62ec7a9d5a33d
olivier5741 <olivier5741@gmail.com>
2018-07-22 13:12:38 +0200

    first search

---
+++ README.md
@@ -0,0 +1,16 @@
+run `npm start` once
+
+run `node server.js`
+
+go to http://localhost:4000/graphql
+
+search
+
+```graphql
+{
+  politicians {
+    id
+    key
+  }
+}
+```
--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const {buildSchema} = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 let schema = buildSchema(`
   type Query {
-    hello: String
+    politicians: [Politician]!
+  }
+  
+  type Politician {
+    id: String
+    key: String
+    firstName: String
+    lastName: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 let root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+    politicians: () => {
+        return [
+            {
+                id: "1421",
+                key: "o.wouters",
+                firstName: "Olivier",
+                lastName: "Wouters"
+            },
+            {
+                id: "1422",
+                key: "d.sum",
+                firstName: "Daniel",
+                lastName: "Sum"
+            }];
+    },
 };
 
 let app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
