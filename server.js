73f5455ed14db7cb27cdc79e80c3022f73c1ee2d
olivier5741 <olivier5741@gmail.com>
2018-07-22 12:58:50 +0200

    ecma 6

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+let schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
+let root = {
   hello: () => {
     return 'Hello world!';
   },
 };
 
-var app = express();
+let app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
